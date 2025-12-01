f1f99c9fcf9206cc1c03427ce354918004638d49
Artwox <Yuttuh2019@gmail.com>
2025-12-01 18:41:13 +0300

    Добавил решение для 3 Задач из практики со строками

---
+++ practices/practice3/task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите данные: ");
+
+        string s = Console.ReadLine();
+
+        string res = "";
+
+        int startb = 0;
+        int endb = 0;
+
+
+        //Дана строка с любыми вложенными скобками: "a(bc(de)f)g(h(i))"
+        //Нужно удалить только содержимое скобок, сами скобки тоже убрать: Вывод: "ag"
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (s[i] == '(')
+            {
+                startb += 1;
+            }
+            else if (s[i] == ')')
+            {
+
+                endb += 1;
+                if (endb > startb)
+                {
+                    Console.WriteLine("Ошибка: лишняя закрывающая скобка!");
+                    return;
+                }
+            }
+            else
+            {
+                if (startb == endb)
+                {
+                    res += s[i];
+                }
+            }
+        }
+
+
+        if (startb != endb)
+        {
+            Console.WriteLine($"Ошибка: не все скобки закрыты! startb={startb}, endb={endb}");
+        }
+        else
+        {
+            Console.WriteLine(res);
+        }
+    }
+}
