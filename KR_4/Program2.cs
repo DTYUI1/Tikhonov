c60bfc2d4d53c0545d28534276b081c91265bd67
Artwox <Yuttuh2019@gmail.com>
2025-12-09 14:57:12 +0300

    КР4
    Добавил решение практических задач на КР4

---
+++ KR_4/Program2.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Создание объекта");
+        object obj = new object();
+        int gen = GC.GetGeneration(obj); 
+        long memory = GC.GetTotalMemory(false);  
+        Console.WriteLine(memory.ToString()); // Занятая память до вызова сборщика мусора
+
+        GC.Collect(0); // Убираем нулевое поколение
+        memory = GC.GetTotalMemory(false);
+        Console.WriteLine(memory.ToString());
+        
+        GC.Collect(1); // Уюирает первое поколение
+        Console.WriteLine(obj.GetType().Name);
+        memory = GC.GetTotalMemory(false);
+        Console.WriteLine(memory.ToString());
+
+        obj = null;
+        GC.Collect(2); // Убираем второе поколение
+
+        GC.WaitForPendingFinalizers();
+        //Console.WriteLine(obj.GetType().Name); - выведет ошибку, т.к сборщик отчистит нуловый object
+
+
+        memory = GC.GetTotalMemory(false); 
+        Console.WriteLine(memory.ToString()); // Занятая память после вызова сборщика мусора
+
+
+        // Выводе можно заметить, что после каждого вызова сборщика мусора, всё меньше и меньше используется памяти, что и логично
+    }
+
+
+
+}
+
+
---
+++ KR_4/Program3.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Security.Cryptography;
+
+class Program
+{
+    static void Main()
+    {
+        int N = Convert.ToInt32(Console.ReadLine());
+        Thread thread = new Thread(() =>
+        {
+            for (int i = 1; i <= N; i++)
+            {
+                Console.WriteLine(i);
+            }
+        });
+
+
+        thread.Start();
+        thread.Join();
+    }
+
+}
